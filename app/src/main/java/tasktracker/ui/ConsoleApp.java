8b5207027e250ffcc5e8fb7659052574f280a24b
danil <danek1412@yandex.ru>
2026-09-09 17:18:13 +0300

    Fixing bugs and errors with building

--- app/src/main/java/tasktracker/ui/ConsoleApp.java
+++ app/src/main/java/tasktracker/ui/ConsoleApp.java
@@ -4,41 +4,51 @@ import tasktracker.service.TaskService;
 import java.util.Scanner;
 
 public class ConsoleApp {
+    
     private final TaskService service;
     private final CommandDispatcher dispatcher;
     private final Scanner scanner;
     private boolean running;
     
-    public ConsoleApp(TaskService service){
+    public ConsoleApp(TaskService service) {
         this.service = service;
         this.dispatcher = new CommandDispatcher();
         this.scanner = new Scanner(System.in);
         this.running = true;
     }
     
-    public void start(){
+    public void start() {
         printWelcome();
         
-        while (running){
+        while (running) {
             System.out.print("task-cli> ");
-            String input = scanner.nextLine().trim();
             
-            if (input.equalsIgnoreCase("exit")){
-                running = false;
+            if (scanner.hasNextLine()) {
+                String input = scanner.nextLine().trim();
+                
+                if (input.equalsIgnoreCase("exit")) {
+                    running = false;
+                    break;
+                }
+                
+                if (!input.isEmpty()) {
+                    dispatcher.execute(input, service);
+                }
+            } else {
+                System.out.println(" No input available. Exiting...");
+                break;
             }
-            
-            dispatcher.execute(input, service);
         }
         
-        System.out.println("Goodbye!");
+        System.out.println("  Goodbye!");
         scanner.close();
     }
     
-    private void printWelcome(){
-        System.out.println("========================================");
-        System.out.println("  JAVA TASK TRACKER   ");
-        System.out.println("========================================");
+    private void printWelcome() {
+        System.out.println("===========================================");
+        System.out.println("    JAVA TASK TRACKER");
+        System.out.println("===========================================");
         System.out.println("Type 'help' for available commands.");
-        System.out.println("========================================\n");
+        System.out.println("===========================================\n");
     }
 }
